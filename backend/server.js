da3df45fa8c77ed60285915659ab761976ff3605
Antim-Tiwari <shikharkant25@gmail.com>
2026-03-15 11:09:03 +0530

    cors issue fixed

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,9 @@ import journalRoutes from './routes/journalRoutes.js'
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 connectDB();
